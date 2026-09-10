dc75733b3e82a935166b6f7639ec535c6412fbe3
epickard-uva <elle.pickard@gmail.com>
2026-09-10 10:35:27 -0400

    Update main_Elle.py

--- main_Elle.py
+++ main_Elle.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-df = pd.read_csv('Metadata and Protein Data for Module 1.csv')
+df = pd.read_csv('/Users/ellepickard/Documents/Computational BME/Module 1/Module1_CompBME/Metadata and Protein Data for Module 1.csv')
 
 for header in df.columns: 
         print(header)
